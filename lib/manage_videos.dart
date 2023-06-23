6a30e92aca993ab70c85a67ab8b247c82cb96131
harianas01 <Hari.Anasuri@oldcastle.com>
2023-06-23 19:46:07 +0530

    1. Completed changes to view the videos based on swipe, but the chewie looks like there are some issues. 2. So will implement better player.

--- lib/manage_videos.dart
+++ lib/manage_videos.dart
@@ -1,11 +1,10 @@
 import 'dart:async';
 
+import 'package:chewie/chewie.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/shared_values.dart';
 import 'package:video_player/video_player.dart';
-import 'package:chewie/chewie.dart';
 
-import 'VideoListItem.dart';
 import 'model/Video.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
@@ -16,137 +15,110 @@ class ManageVideos extends StatefulWidget {
   @override
   State<ManageVideos> createState() => _ManageVideosState();
 }
-int optionSelected = 0;
+
 class _ManageVideosState extends State<ManageVideos> {
-  late Future<List<Video>> _videoListFuture;
+  List<Video>? videoList;
+  VideoPlayerController? _videoPlayerController;
+  late ChewieController _chewieController;
+  int currentIndex = 0;
 
   @override
   void initState() {
     super.initState();
-      _videoListFuture = VideoProvider.fetchVideos();
-    }
+    VideoProvider.fetchVideos().then((videos) {
+      setState(() {
+        debugPrint("Setting videos:${videos?.length}");
+        videoList = videos;
+        setVideo(currentIndex,true);
+      });
+    });
+  }
 
   @override
   void dispose() {
-    // Ensure disposing of the VideoPlayerController to free up resources.
+    // Ensure disposig of the VideoPlayerController to free up resources.
+    _videoPlayerController?.dispose();
+    _chewieController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Butterfly Video'),
+    if (videoList == null) {
+      return const Center(child: CircularProgressIndicator());
+    } else {
+      return
+        Scaffold(
+          backgroundColor: Colors.grey,
+          body: GestureDetector(
+          onHorizontalDragEnd: (DragEndDetails details) {
+            if (details.primaryVelocity! < 0) {
+              // Swiped left, play next video
+              if (currentIndex < videoList!.length - 1) {
+                playNewVideo(currentIndex + 1, true);
+              }
+            } else if (details.primaryVelocity! > 0) {
+              // Swiped right, play previous video
+              if (currentIndex > 0) {
+                playNewVideo(currentIndex - 1, false);
+              }
+            }
+          },
+          child: Chewie(
+            controller: _chewieController,
+          ),
       ),
-      // Use a FutureBuilder to display a loading spinner while waiting for the
-      // VideoPlayerController to finish initializing.
-      body: FutureBuilder<List<Video>>(
-        future: _videoListFuture,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Center(child: Text('Failed to load videos'));
-          } else {
-            final videoList = snapshot.data!;
-            return ListView.builder(
-              itemCount: videoList.length,
-              itemBuilder: (context, index) {
-                return VideoListItem(video: videoList[index]);
-              },
-            );
-          }
-        },
-      ),
-      bottomNavigationBar: NavigationBar(
-        height: 50,
-        backgroundColor: Colors.green[100],
-        surfaceTintColor: Colors.red,
-        destinations: const [
-          NavigationDestination(
-              icon: Icon(Icons.calendar_today, color: Colors.blue), label: 'Date'),
-          NavigationDestination(
-              icon: Icon(Icons.refresh, color: Colors.blue), label: 'Refresh'),
-        ],
-        onDestinationSelected: (int index) async {
-          if (index == 0) {
-            //_showPopup(context);
-            _datePicker(context);
-          }
-          if (index == 1) {
-            _videoListFuture = VideoProvider.fetchVideos();
-          }
-          setState(() {
-            optionSelected = index;
+        );
+    }
+  }
+  void playNewVideo(int newIndex, bool isNext) {
+    setState(() {
+      if(newIndex == (videoList!.length -1)){
+        if(isNext){
+          debugPrint("Getting next set");
+          AppValues.dateForVideos = videoList![videoList!.length-1].createdDate;
+          VideoProvider.fetchVideos().then((videos) {
+            videoList = videos;
+          });
+        }else{
+          debugPrint("Getting previous set");
+          AppValues.dateForVideos = videoList![videoList!.length-1].createdDate;
+          VideoProvider.fetchVideos().then((videos) {
+            videoList = videos;
           });
         }
-      ),
-    );
+        currentIndex = 0;
+        setVideo(currentIndex, false);
+      }else{
+        debugPrint("Setting next video");
+        setVideo(newIndex, false);
+      }
+    });
   }
-  void _datePicker(BuildContext context) async{
-    DateTime selectedDate;
-    if(AppValues.importantPhotosDate.isNotEmpty){
-      selectedDate = DateTime.parse(AppValues.importantPhotosDate);
-    }else{
-      selectedDate = DateTime.now();
+  void setVideo(int newIndex, isItFirtLoad){
+    currentIndex = newIndex;
+    debugPrint("videoList![currentIndex].videoUrl: ${videoList![currentIndex].videoUrl}");
+
+    if(!isItFirtLoad){
+      debugPrint("Disposing the video");
+      _videoPlayerController?.dispose();
+      _chewieController.dispose();
+
     }
-    DateTime? pickedDate = await showDatePicker(
-        context: context, //context of current state
-        initialDate: selectedDate,
-        firstDate: DateTime(1990), //DateTime.now() - not to allow to choose before today.
-        lastDate: DateTime(2101)
-    );
+    _videoPlayerController = VideoPlayerController.network(videoList![currentIndex].videoUrl);
+    _chewieController = ChewieController(
+      videoPlayerController: _videoPlayerController!,
+      allowedScreenSleep: false,
+      allowPlaybackSpeedChanging: true,
+      zoomAndPan: true,
+      //fullScreenByDefault: true,
+      //aspectRatio: _videoPlayerController!.value.aspectRatio,
+      autoInitialize: true,
 
-    if(pickedDate != null ){
-      print(pickedDate);  //pickedDate output format => 2021-03-10 00:00:00.000
-      AppValues.dateForVideos = pickedDate.toString();
-      //String formattedDate = DateFormat('yyyy-MM-dd').format(pickedDate);
-      //print(formattedDate); //formatted date output using intl package =>  2021-03-16
-    }else{
-      print("Date is not selected");
-    }
-  }
-  void _showPopup(BuildContext context) {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: const Text('Enter Text'),
-          content: TextField(
-            controller: TextEditingController(text: AppValues.importantPhotosDate),
-            //controller: TextEditingController(text: "2013-02-29 14:22:22.0"),
-
-            onChanged: (value) {
-              setState(() {
-                AppValues.setImportantPhotosDate(value);
-              });
-            },
-            /*decoration: const InputDecoration(
-              hintText: 'Type something...',
-            ),*/
-
-          ),
-          actions: <Widget>[
-            TextButton(
-              child: Text('Cancel'),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
-            TextButton(
-              child: Text('Save'),
-              onPressed: () {
-                // Perform the save operation here
-                Navigator.of(context).pop();
-              },
-            ),
-          ],
-        );
-      },
+      // Add any other Chewie options you need
     );
   }
 }
-
 class VideoProvider {
   static Future<List<Video>> fetchVideos() async {
     final response = await http.get(Uri.parse(AppValues.getNextSetOfVideosInfo()));
@@ -165,3 +137,4 @@ class VideoProvider {
     }
   }
 }
+
